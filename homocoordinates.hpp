5438fcde07afdf116c17923c8022db37b049de16
sunrisepeak <speakshen@163.com>
2021-12-17 15:33:54 +0800

    from int to unsigned int in for
    Signed-off-by: sunrisepeak <speakshen@163.com>

--- homocoordinates.hpp
+++ homocoordinates.hpp
@@ -8,14 +8,19 @@ namespace TMATH {
 template <unsigned int N = 3>
 class HomoCoordinates : public Vector<double, N> {
 public:
+
     HomoCoordinates() {
 
+    }
+
+    HomoCoordinates(const Vector<double, N> &v) {
+        *this = v;
     }
 
     template <typename... Targs>
     HomoCoordinates(const double n, const Targs... ns) : Vector<double, N>(n, ns...) {
         if ((*this)[N -1] != 0) {
-            for (int i = 0; i < N; i++) {
+            for (unsigned int i = 0; i < N; i++) {
                 (*this)[i] = (*this)[i] / (*this)[N -1];
             }
         }
